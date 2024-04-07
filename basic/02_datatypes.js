9d4228a96bd855bcafae1f352176df5313fcd737
Anjaly Thandel <111124506+Anjaly997@users.noreply.github.com>
2024-04-07 12:32:15 +0000

    datatypes

---
+++ basic/02_datatypes.js
@@ -0,0 +1,21 @@
+ "use strict"; //treat all the JS code as newer version
+
+//  alert(4 +8) // we are using nodejs not browser
+
+let name ="anjaly"
+let age = 25
+let isLoggedIn = true
+
+
+// number =>2 to power 53
+// bigint
+// string
+// boolean =>true/false
+// null => standalone
+// undefined =>
+// symbol =>unique
+
+// object
+
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); //object
