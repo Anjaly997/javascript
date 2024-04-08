97a075710f6cd5427eb12cd9d83c16f0725b8c07
Anjaly Thandel <111124506+Anjaly997@users.noreply.github.com>
2024-04-08 12:29:01 +0000

    Datatypes

---
+++ basic/datatypes-summary.js
@@ -0,0 +1,18 @@
+// Primitive 
+
+// 7 types : String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score  = 100
+const scoreValue =100.3 //numbertype
+const isLoggedIn = false // boolean
+const outsideTemp = null
+let userEmail;
+
+const id =Symbol('123')
+const anotherId= Symbol('123')
+
+console.log(id === another);
+
+// Reference (Non-primitive)
+
+// Array,Objects,Functions
