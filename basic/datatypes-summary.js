fa05fc86d78c9e5a6ad58ac937b2605280a39a61
Anjaly Thandel <111124506+Anjaly997@users.noreply.github.com>
2024-04-08 17:29:25 +0000

    datatypes

--- basic/datatypes-summary.js
+++ basic/datatypes-summary.js
@@ -11,8 +11,27 @@ let userEmail;
 const id =Symbol('123')
 const anotherId= Symbol('123')
 
-console.log(id === another);
+console.log(id === anotherId);
+
+
+const bigNumber = 3456789000876543231n
+
+// console.log(bigNumber);
 
 // Reference (Non-primitive)
 
 // Array,Objects,Functions
+
+const heros =["ninga","shakti","doja"] //array
+let mjObj ={
+ name: "ann",
+ age:24,
+}// object
+
+
+const myFunction = function(){
+    console.log("Hello")
+}// functions
+
+console.log(typeof bigNumber);
+ 
